c45bdc5483c55fc8512a7f0d8ec74b7ca6c844e5
aaaro <aaaro@DESKTOP-GQUVVON>
2023-09-03 14:59:34 +0200

    Initial Commit And Setup.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Header from "./components/Header.vue";
+  import About from "./components/About.vue";
+</script>
+
+<template>
+  <Header/>
+  <About/>
+</template>
+
+<style>
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+        <div class="header">
+            <img src="./../assets/image.png">
+        </div>
+        <div class="content">
+            <h1>Aaron Sanchez Wolters</h1>
+            <h3>Software Developer</h3>
+        </div>
+        </div>
+</template>
+
+<style>
+  .header{    
+    background-color:black; 
+    width:100%; 
+    padding: 0 0 30%;
+    position: relative;
+  }
+
+  .content{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 7%;
+  }
+
+  img{
+    width:30%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 20%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
