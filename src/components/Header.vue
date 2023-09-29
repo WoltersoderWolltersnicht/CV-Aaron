dd8b5afd4e2c2f7cd801cfb66c07dd606e71d50f
WoltersoderWolltersnicht <aaaronwolters@gmail.com>
2023-09-29 23:03:56 +0200

    Improved Download CV Button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,10 +9,6 @@
       <img class="mt-20 w-48 md:w-80"  src="./../assets/image.png">
       <h1 class="text-xl sm:text-5xl font-bold text-white">{{personalData.name}}</h1>
       <h2 class="mt-2 text-lg sm:text-3xl text-white">{{personalData.possition}}</h2>
-      <a href="../assets/CV_AaronSanchezWolters" download="CV_AaronSanchezWolters.pdf" >
-        <button class="mt-2 align-baseline h-10 bg-accent-base border border-accent-border p-2 pb-8 rounded-full" href="../assets/CV_AaronSanchezWolters.pdf" download="CV_AaronSanchezWolters">Download CV</button>
-      </a>
-
       <ContactInformation :contact="personalData.contact"/>
       <About :about ="personalData.about"/>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,10 +22,13 @@ export default {
 
 <template>
     <nav class="bg-dark-header fixed w-screen h-16">
-        <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4 sm:p-1">
-            <div :class="[{ 'my-hidden': !menuOpen }]" class="w-full mr-5 sm:!block sm:w-auto sm:!visible sm:mx-auto" id="navbar-default">
+        <a href="../assets/CV_AaronSanchezWolters" download="CV_AaronSanchezWolters.pdf" >
+            <button type="button" class="fixed m-4  flex bg-accent-base border border-accent-border font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0" href="../assets/CV_AaronSanchezWolters.pdf" download="CV_AaronSanchezWolters">Download CV</button>
+        </a>
+        <div class="max-w-screen-xl flex flex-row justify-end mx-auto p-4 sm:justify-between sm:p-1 h-16">
+            <div :class="[{ 'my-hidden': !menuOpen }]" class="mt-10 sm:!block sm:w-auto sm:mt-0 sm:!visible sm:mx-auto sm:self-center" id="navbar-default">
                 <ul
-                    class="font-medium flex flex-col p-4 sm:p-0 border rounded-lg sm:flex-row sm:space-x-8 sm:border-0 bg-dark-header sm:bg-dark-header border-gray-700">
+                    class="font-medium flex flex-col sm:p-0 border rounded-lg sm:flex-row sm:space-x-8 sm:border-0 bg-dark-header sm:bg-dark-header border-gray-700">
                     <li>
                         <a href="#" @click="selectMenuItem"
                             class="block py-2 pl-3 pr-4  rounded  sm:border-0  sm:p-0 text-white sm:hover:text-accent-base hover:bg-gray-700 hover:text-accent-base sm:hover:bg-transparent"
@@ -47,7 +50,7 @@ export default {
                 </ul>
             </div>
             <button data-collapse-toggle="navbar-default" type="button" @click="openMenu"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg sm:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
+                class="inline-flex items-center w-10 h-10 justify-center text-sm rounded-lg sm:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
                 aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
